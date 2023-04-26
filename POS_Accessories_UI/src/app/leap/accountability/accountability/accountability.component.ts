9dc96f2e00e3b464fa03b0c14f68e69f99fba85f
Nalluri <rnalluri@rcinnet.rockwellcollins.com>
2023-04-26 15:08:37 +0530

    Accountability page with sample dynamic add

--- POS_Accessories_UI/src/app/leap/accountability/accountability/accountability.component.ts
+++ POS_Accessories_UI/src/app/leap/accountability/accountability/accountability.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, 
+  FormGroup,
+  FormBuilder,
+  FormArray,
+  Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-accountability',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountabilityComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  buyTicketForm: FormGroup;
+  constructor(private fb: FormBuilder) {
   }
 
+  createTicket():FormGroup{
+
+    return this.fb.group({
+      name:[null,Validators.required],
+      age:[null,Validators.required]
+    })
+  }
+
+  ngOnInit() {
+    this.buyTicketForm = this.fb.group(
+      {
+        emailControl: [null, [Validators.required]],
+        phoneControl: [null],
+        address:this.fb.group({
+          streetControl : [],
+          postalcodeControl: []
+        }),
+        tickets:this.fb.array([this.createTicket()],Validators.required)
+      }
+    )
+  }
+  get tickets():FormArray{
+    return <FormArray> this.buyTicketForm.get('tickets');
+  }
+  
+  addTicket() {
+    this.tickets.push(this.createTicket());
+  }
+
+  buyTickets() {
+
+    if(this.buyTicketForm.status == 'VALID'){
+      console.log(this.buyTicketForm.value);
+    }
+  }
 }
